66481d244fdcf88ea35825a3ee0f095938adeeb6
tquetano-r7 <tony_quetano@rapid7.com>
2016-10-12 16:34:57 -0400

    Updates... * Allow strings and such to have max-len ignored

--- rules/legacy.js
+++ rules/legacy.js
@@ -4,7 +4,13 @@ module.exports = {
   },
   rules: {
     'max-depth': [1, 6],
-    'max-len': [1, 120, 4],
+    'max-len': [1, 120, {
+      'ignoredComments': true,
+      'ignoreTrailingComments': true,
+      'ignoreUrls': true,
+      'ignoreStrings': true,
+      'ignoreTemplateLiterals': true
+    }],
     'max-statements': [1, 50],
     'no-bitwise': 1
   }
