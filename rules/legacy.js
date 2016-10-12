77e976490e292da478b38b189c82b359098cb60b
tquetano-r7 <tony_quetano@rapid7.com>
2016-10-12 16:41:23 -0400

    Updates... * Loosen max-len rules

--- rules/legacy.js
+++ rules/legacy.js
@@ -4,13 +4,9 @@ module.exports = {
   },
   rules: {
     'max-depth': [1, 6],
-    'max-len': [1, {
+    'max-len': [2, {
       'code': 120,
-      'ignoredComments': true,
-      'ignoreTrailingComments': true,
-      'ignoreUrls': true,
-      'ignoreStrings': true,
-      'ignoreTemplateLiterals': true
+      'ignoreStrings': true
     }],
     'max-statements': [1, 50],
     'no-bitwise': 1
