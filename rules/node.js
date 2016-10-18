9523c0c8a0528dbf55e922eeb3f551c5a3c4553b
John Manero <jmanero@rapid7.com>
2016-10-18 13:09:00 -0400

    Add node-specific rules and configuration
    * Remove JSX profile and disallow JSX
    * Set sourceType to script. NodeJS 4.x does not support
      import/export module definition
    * Do not require braces for one-line arrow functions, do require space
      inside of braces
    * Require 2-space indentation, UNIX line-breaks, and
      normalized whitespace
    * Require global strict mode everywhere
    * Require JSDoc for all functions, methods, and classes

--- rules/node.js
+++ rules/node.js
@@ -1,6 +1,36 @@
+/*
+* NodeJS 4.x:
+* * Supports CommonJS module/require
+* * Does not support import/export
+* * Does not support JSX (a browser-targeted feature)
+* * Does not support destructuring or spread operators
+*/
 module.exports = {
   env: {
     node: true
   },
-  rules: {}
+  parserOptions: {
+    ecmaFeatures: {
+      globalReturn: false,
+      jsx: false,
+      experimentalObjectRestSpread: false
+    },
+    sourceType: 'script'
+  },
+  rules: {
+    'arrow-body-style': 0,
+    'block-spacing': [1, 'always'],
+    indent: [1, 2],
+    'linebreak-style': [1, 'unix'],
+    'no-trailing-spaces': 1,
+    'no-irregular-whitespace': 1,
+    strict: [2, 'global'],
+    'require-jsdoc': [1, {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: true
+      }
+    }]
+  }
 };
