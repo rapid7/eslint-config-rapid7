da2975d12cd279f7de77271831edb228f1814a5d
John Manero <jmanero@rapid7.com>
2016-10-19 10:29:03 -0400

    Remove arrow-style override

--- rules/node.js
+++ rules/node.js
@@ -1,9 +1,15 @@
 /*
 * NodeJS 4.x:
-* * Supports CommonJS module/require
-* * Does not support import/export
-* * Does not support JSX (a browser-targeted feature)
-* * Does not support destructuring or spread operators
+* - Supports CommonJS module/require
+* - Does not support import/export
+* - Does not support destructuring or spread operators
+* - Requires global `strict` mode for class and block-scoped declarations
+*
+* Notes:
+* - UNIX whitespace is desired to ensure correct parsing on all
+*   platforms. NodeJS-target JavaScript needs to be evaluated by a shell
+*   when an executable script has a shebang header, and `\m` whitespace can
+*   break this in strange ways.
 */
 module.exports = {
   env: {
@@ -17,14 +23,13 @@ module.exports = {
     sourceType: 'script'
   },
   rules: {
-    'arrow-body-style': 0,
+    strict: [2, 'global'],
     'block-spacing': [1, 'always'],
     indent: [1, 2],
     'linebreak-style': [1, 'unix'],
     'no-trailing-spaces': 1,
     'no-irregular-whitespace': 1,
     'prefer-spread': 0,
-    strict: [2, 'global'],
     'require-jsdoc': [1, {
       require: {
         FunctionDeclaration: true,
