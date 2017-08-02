75a1de96efe2977d0235ee47d13016da1ba486fb
Alex Rosenthal <Alex_Rosenthal@rapid7.com>
2017-08-02 10:59:37 -0400

    make jsdoc rules enforce type preference

--- rules/errors.js
+++ rules/errors.js
@@ -5,6 +5,14 @@ module.exports = {
     'comma-dangle': [2, 'never'],
     'no-invalid-regexp': 2,
     'valid-jsdoc': [1, {
+      preferType: {
+        array: 'Array',
+        Boolean: 'boolean',
+        Function: 'function',
+        Number: 'number',
+        object: 'Object',
+        String: 'string'
+      },
       requireReturn: false,
       requireParamDescription: false,
       requireReturnDescription: false
