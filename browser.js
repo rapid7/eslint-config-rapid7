ef5b79cf412c78d6ecf93ed31b5ab8f700783e99
Tony Quetano <tony_quetano@rapid7.com>
2018-06-13 10:14:54 -0400

    Incorporate base as part of environment-specific configs
    So that you do not need to compose them with base (as you always would

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint-config-rapid7/rules/browser'].map(require.resolve),
+  extends: ['eslint-config-rapid7/base', 'eslint-config-rapid7/rules/browser'].map(require.resolve),
   env: {
     browser: true,
     node: false,
--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint-config-rapid7/rules/node'].map(require.resolve),
+  extends: ['eslint-config-rapid7/base', 'eslint-config-rapid7/rules/node'].map(require.resolve),
   env: {
     browser: false,
     node: true,
