b604741beae2b7c2f56297fc978d1e8295e57d87
Tony Quetano <tony_quetano@rapid7.com>
2018-06-13 10:58:26 -0400

    Add no-magic-numbers rule
    Resolves #29

--- rules/strict.js
+++ rules/strict.js
@@ -2,6 +2,13 @@ module.exports = {
   rules: {
     'max-depth': [1, 1],
     'max-statements': [1, 25],
+    'no-magic-numbers': [
+      2,
+      {
+        enforceConst: true,
+        ignoreArrayIndexes: true,
+      },
+    ],
     'one-var': [2, 'never'],
     'sort-keys': [2, 'asc', {caseSensitive: false, natural: true}],
     'valid-jsdoc': [
