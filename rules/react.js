7c0d4cc8d344f8eceac739606532ec25c3d95500
Tony Quetano <tony_quetano@rapid7.com>
2018-06-20 10:14:30 -0400

    upgrade-rapid7-plugin

--- rules/react.js
+++ rules/react.js
@@ -32,12 +32,7 @@ module.exports = {
     'react/jsx-no-target-blank': 2,
     'react/jsx-no-undef': 1,
     'react/jsx-pascal-case': 2,
-    'react/jsx-sort-props': [
-      2,
-      {
-        ignoreCase: true,
-      },
-    ],
+    'react/jsx-sort-props': 2,
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
     'react/jsx-wrap-multilines': 2,
