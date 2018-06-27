0296b1acb73c159e169af7376a7a6c6839576af4
Tony Quetano <tquetano-r7@users.noreply.github.com>
2018-06-27 15:10:57 -0400

    Add missing rules for strict setup (#39)
    * func-style (prefer expressions)
    * no-param-reassign
    * prefer-destructuring (object only)
    * valid-jsdoc (make error, and provide more detailed application)

--- rules/strict.js
+++ rules/strict.js
@@ -1,5 +1,7 @@
 module.exports = {
   rules: {
+    'func-style': [2, 'expression', {allowArrowFunctions: true}],
+    'line-comment-position': [2, {position: 'above'}],
     'max-depth': [1, 1],
     'max-statements': [1, 25],
     'no-magic-numbers': [
@@ -9,12 +11,37 @@ module.exports = {
         ignoreArrayIndexes: true,
       },
     ],
+    'no-param-reassign': [2, {props: true}],
     'one-var': [2, 'never'],
+    'prefer-destructuring': [
+      2,
+      {
+        array: false,
+        object: true,
+      },
+    ],
     'sort-keys': [2, 'asc', {caseSensitive: false, natural: true}],
     'valid-jsdoc': [
-      1,
+      2,
       {
+        prefer: {
+          argument: 'arg',
+          class: 'constructor',
+          return: 'returns',
+        },
+        preferType: {
+          Boolean: 'boolean',
+          Function: 'function',
+          Null: 'null',
+          Number: 'number',
+          object: 'Object',
+          regexp: 'RegExp',
+          promise: 'Promise',
+          String: 'string',
+          Undefined: 'undefined',
+        },
         requireReturn: false,
+        requireReturnType: true,
         requireParamDescription: true,
         requireReturnDescription: true,
       },
